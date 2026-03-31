3f93455a749c86f1083336ffb7fe8735e389702a
Patryk Orłowski <orlowski.dev@gmail.com>
2026-03-31 08:45:19 +0200

    ft(logger): Tworzy lub zmienia plik log
    Jeśli plik nie istnieje to tworzy go i wpisuje tekst, a jeśli istnieje
    to dopisuje content na koniec pliku.

--- Scripts/Utils/FileSystem.cs
+++ Scripts/Utils/FileSystem.cs
@@ -2,9 +2,22 @@ using Godot;
 
 public static class FileSystem
 {
-    public static void Write(string path, string content)
-    {
-        using var file = FileAccess.Open(path, FileAccess.ModeFlags.Write);
-        file.StoreString(content);
-    }
+	public static void Write(string path, string content)
+	{
+		var fileExists = false;
+
+		if (FileAccess.FileExists(path))
+		{
+			fileExists = true;
+		}
+
+		// ustawia flagę otwarcia pliku - jak plik istnieje to dopisz a jak nie
+		// to utwórz i wpisz
+		var flag = fileExists ? FileAccess.ModeFlags.ReadWrite : FileAccess.ModeFlags.Write;
+
+		using var file = FileAccess.Open(path, flag);
+		if (fileExists)
+			file.SeekEnd(); // ustawia kursor na koniec
+		file.StoreString(content);
+	}
 }

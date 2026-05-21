a9b7a823c2c7d61fc4a8edd5e0b01fdffa22e019
Patryk Orłowski <orlowski.dev@gmail.com>
2026-05-21 18:24:37 +0200

    ft(player-spawn): add player spawn on generated dung

--- Game.Node/Scripts/Controllers/DungGeneratorController.cs
+++ Game.Node/Scripts/Controllers/DungGeneratorController.cs
@@ -7,8 +7,10 @@ using Godot;
 public partial class DungGeneratorController : Node
 {
     private Logger Logger => Logger.Instance;
+    private GameManager GameManager => GameManager.Instance;
     private DungGeneratorService _service;
     private TileMapLayer _tileMapLayer;
+    private Point _playerSpawnPointPos = new(50, 50);
 
     /// <summary>
     /// Wywoływane po załadowaniu sceny.
@@ -28,5 +30,23 @@ public partial class DungGeneratorController : Node
             // Ustawienie komórki na TileMapLayer: (pozycja, atlas_coord, src_rect, autotile_coord)
             _tileMapLayer.SetCell(coords, 0, tileCoords, 0);
         }
+
+        SpawnPlayer();
+    }
+
+    /// <summary>
+    /// Tworzy instancję postaci gracza i kamery, dodając je do drzewa sceny.
+    /// Pobiera odpowiednie sceny z <see cref="GameManager"/> na podstawie wybranej klasy postaci.
+    /// </summary>
+    private void SpawnPlayer()
+    {
+        var playerNode = GameManager.GetPlayerNode(
+            GameManager.PlayerCharacter.CharacterClass.NodeName,
+            CoordMapper.ToVector2I(_playerSpawnPointPos)
+        );
+        var cameraNode = GameManager.GetPlayerCameraNode();
+
+        AddChild(playerNode);
+        AddChild(cameraNode);
     }
 }
